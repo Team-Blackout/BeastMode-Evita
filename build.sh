34dc953cfff1a5a1078587325a589c7080221463
sbryan12144 <sbryan12144@BlackBOX.(none)>
2012-09-24 16:29:00 -0400

    Clean Up

--- build.sh
+++ build.sh
@@ -6,38 +6,74 @@ echo "============================================================"
 echo "                         Cleaning                           "
 echo "============================================================"
 make clean mrproper
-defconfig=21
+defconfig=21gpu
 echo "============================================================"
-echo "             Compiling Evita Kernel at 2.1GHz               "
+echo "       Compiling Evita Kernel at 2.1GHz With GPU OC         "
 echo "============================================================"
-make beastmode21_defconfig 
+make beastmode21gpu_defconfig 
 make -j`grep 'processor' /proc/cpuinfo | wc -l`
 ./copy.sh $defconfig;
 clear
 make clean mrproper
-defconfig=18
+defconfig=21ngpu
 echo "============================================================"
-echo "             Compiling Evita Kernel at 1.89GHz               "
+echo "      Compiling Evita Kernel at 2.1GHz Without GPU OC       "
 echo "============================================================"
-make beastmode18_defconfig 
+make beastmode21ngpu_defconfig 
 make -j`grep 'processor' /proc/cpuinfo | wc -l`
 ./copy.sh $defconfig;
 clear
 make clean mrproper
-defconfig=17
+defconfig=18gpu
 echo "============================================================"
-echo "             Compiling Evita Kernel at 1.72GHz               "
+echo "       Compiling Evita Kernel at 1.89GHz With GPU OC        "
 echo "============================================================"
-make beastmode17_defconfig 
+make beastmode18gpu_defconfig 
 make -j`grep 'processor' /proc/cpuinfo | wc -l`
 ./copy.sh $defconfig;
 clear
 make clean mrproper
-defconfig=15
+defconfig=18ngpu
 echo "============================================================"
-echo "             Compiling Evita Kernel at 1.51GHz               "
+echo "      Compiling Evita Kernel at 1.89GHz Without GPU OC      "
 echo "============================================================"
-make beastmode15_defconfig 
+make beastmode18ngpu_defconfig 
+make -j`grep 'processor' /proc/cpuinfo | wc -l`
+./copy.sh $defconfig;
+clear
+make clean mrproper
+defconfig=17gpu
+echo "============================================================"
+echo "       Compiling Evita Kernel at 1.72GHz With GPU OC        "
+echo "============================================================"
+make beastmode17gpu_defconfig 
+make -j`grep 'processor' /proc/cpuinfo | wc -l`
+./copy.sh $defconfig;
+clear
+make clean mrproper
+defconfig=17ngpu
+echo "============================================================"
+echo "      Compiling Evita Kernel at 1.72GHz Without GPU OC      "
+echo "============================================================"
+make beastmode17ngpu_defconfig 
+make -j`grep 'processor' /proc/cpuinfo | wc -l`
+./copy.sh $defconfig;
+clear
+make clean mrproper
+defconfig=15gpu
+echo "============================================================"
+echo "       Compiling Evita Kernel at 1.51GHz With GPU OC        "
+echo "============================================================"
+make beastmode15gpu_defconfig 
+make -j`grep 'processor' /proc/cpuinfo | wc -l`
+./copy.sh $defconfig;
+clear
+make clean mrproper
+defconfig=15ngpu
+echo "============================================================"
+echo "      Compiling Evita Kernel at 1.51GHz Without GPU OC      "
+echo "============================================================"
+make beastmode15ngpu_defconfig 
 make -j`grep 'processor' /proc/cpuinfo | wc -l`
 ./copy.sh $defconfig;
 clear
