9e6fbc5d01a5d8a288f49bbb8e67a59c75e62624
zarboz <zarboz@gmail.com>
2012-10-27 00:56:04 -0600

    modified:   arch/arm/mach-msm/Kconfig 	modified:   drivers/input/touchscreen/Kconfig 	modified:   drivers/input/touchscreen/synaptics_3200.c

--- drivers/input/touchscreen/synaptics_3200.c
+++ drivers/input/touchscreen/synaptics_3200.c
@@ -1634,7 +1634,7 @@ static void synaptics_ts_finger_func(struct synaptics_ts_data *ts)
 						if (!ts->first_pressed) {
 							if (ts->finger_count == 0)
 								ts->first_pressed = 1;
-							printk(KERN_INFO "[TP] E%d@%d, %d\n", i + 1,
+							printk(KERN_INFO "[TP1] E%d@%d, %d\n", i + 1,
 							finger_data[i][0], finger_data[i][1]);
 						}
 					}
@@ -1673,9 +1673,11 @@ static void synaptics_ts_finger_func(struct synaptics_ts_data *ts)
 								ts->tap_suppression &= ~BIT(i);
 							} else {
 								finger_pressed &= ~BIT(i);
+								
+
 								if (ts->debug_log_level & 0x2)
 							printk(KERN_INFO
-								"[TP] Filtered Finger %d=> X:%d, Y:%d w:%d, z:%d\n",
+								"[TP2] Filtered Finger %d=> X:%d, Y:%d w:%d, z:%d\n",
 								i + 1, finger_data[i][0], finger_data[i][1],
 								finger_data[i][2], finger_data[i][3]);
 							}
@@ -1734,37 +1736,11 @@ static void synaptics_ts_finger_func(struct synaptics_ts_data *ts)
 								(finger_pressed == 0) << 31 |
 								finger_data[i][0] << 16 | finger_data[i][1]);
 						}
-
-						if (ts->pre_finger_data[0][0] < 2) {
-							if (finger_press_changed & BIT(i)) {
-								ts->pre_finger_data[i + 1][0] = finger_data[i][0];
-								ts->pre_finger_data[i + 1][1] = finger_data[i][1];
-								if (!ts->first_pressed)
-									printk(KERN_INFO "[TP] S%d@%d, %d\n", i + 1,
-										finger_data[i][0], finger_data[i][1]);
-#ifdef SYN_CALIBRATION_CONTROL
-								if (ts->finger_count == ts->finger_support) {
-									ret = i2c_syn_write_byte_data(ts->client,
-										get_address_base(ts, 0x11, COMMAND_BASE), 0x01);
-									if (ret < 0)
-										i2c_syn_error_handler(ts, ts->i2c_err_handler_en, "w:Rezero_1", __func__);
-									printk(KERN_INFO "[TP] %s: Touch Calibration Confirmed, rezero\n", __func__);
-								} else if (!ts->pre_finger_data[0][0] && ts->finger_count > 1)
-									ts->pre_finger_data[0][0] = 1;
-#endif
-							}
-						}
-
-						if (ts->htc_event == SYN_AND_REPORT_TYPE_B && ts->reduce_report_level[TAP_DX_OUTER]) {
-							if (finger_press_changed & BIT(i)) {
-								ts->tap_suppression &= ~BIT(i);
-								ts->tap_suppression |= BIT(i);
-								ts->pre_finger_data[i + 1][2] = finger_data[i][0];
-								ts->pre_finger_data[i + 1][3] = finger_data[i][1];
-								if (ts->reduce_report_level[TAP_TIMEOUT] && (ts->tap_suppression))
-									ts->tap_timeout[i] = jiffies + msecs_to_jiffies(ts->reduce_report_level[TAP_TIMEOUT]);
-							}
 #ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_SWEEP2WAKE
+
+				      printk(KERN_INFO "[sweep2wake]: %d=> X:%d, Y:%d w:%d, z:%d\n",
+								i + 1, finger_data[i][0], finger_data[i][1],
+								finger_data[i][2], finger_data[i][3]);
 							//left->right
 							if ((ts->finger_count == 1) && (scr_suspended == true) && (s2w_switch > 0)) {
 								prevx = 30;
@@ -1828,11 +1804,40 @@ static void synaptics_ts_finger_func(struct synaptics_ts_data *ts)
 								}
 							}
 #endif
+						if (ts->pre_finger_data[0][0] < 2) {
+							if (finger_press_changed & BIT(i)) {
+								ts->pre_finger_data[i + 1][0] = finger_data[i][0];
+								ts->pre_finger_data[i + 1][1] = finger_data[i][1];
+								if (!ts->first_pressed)
+									printk(KERN_INFO "[TP] S%d@%d, %d\n", i + 1,
+										finger_data[i][0], finger_data[i][1]);
+#ifdef SYN_CALIBRATION_CONTROL
+								if (ts->finger_count == ts->finger_support) {
+									ret = i2c_syn_write_byte_data(ts->client,
+										get_address_base(ts, 0x11, COMMAND_BASE), 0x01);
+									if (ret < 0)
+										i2c_syn_error_handler(ts, ts->i2c_err_handler_en, "w:Rezero_1", __func__);
+									printk(KERN_INFO "[TP] %s: Touch Calibration Confirmed, rezero\n", __func__);
+								} else if (!ts->pre_finger_data[0][0] && ts->finger_count > 1)
+									ts->pre_finger_data[0][0] = 1;
+#endif
+							}
 						}
 
+						if (ts->htc_event == SYN_AND_REPORT_TYPE_B && ts->reduce_report_level[TAP_DX_OUTER]) {
+							if (finger_press_changed & BIT(i)) {
+								ts->tap_suppression &= ~BIT(i);
+								ts->tap_suppression |= BIT(i);
+								ts->pre_finger_data[i + 1][2] = finger_data[i][0];
+								ts->pre_finger_data[i + 1][3] = finger_data[i][1];
+								if (ts->reduce_report_level[TAP_TIMEOUT] && (ts->tap_suppression))
+									ts->tap_timeout[i] = jiffies + msecs_to_jiffies(ts->reduce_report_level[TAP_TIMEOUT]);
+							}
+							
+						}
 						if (ts->debug_log_level & 0x2)
 							printk(KERN_INFO
-								"[TP] Finger %d=> X:%d, Y:%d w:%d, z:%d\n",
+								"[TP3] Finger %d=> X:%d, Y:%d w:%d, z:%d\n",
 								i + 1, finger_data[i][0], finger_data[i][1],
 								finger_data[i][2], finger_data[i][3]);
 					}
@@ -2765,26 +2770,6 @@ static int synaptics_ts_resume(struct i2c_client *client)
 {
 	int ret;
 	struct synaptics_ts_data *ts = i2c_get_clientdata(client);
-#ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_SWEEP2WAKE
-	if (s2w_switch > 0) {
-		/* HW revision fix, this is not needed for all touch controllers!
-		 * suspend me for a short while, so that resume can wake me up the right way
-		 *
-		 * --NO IDEA IF THIS IS NEEDED ON THE ONE X, INCLUDE IT TO BE SURE FOR NOW!--
-		 *
-		 */
-		ret = i2c_syn_write_byte_data(client,
-			get_address_base(ts, 0x01, CONTROL_BASE), 0x01); /* sleep */
-		if (ret < 0)
-			i2c_syn_error_handler(ts, 1, "sleep", __func__);
-		msleep(150);
-		ret = 0;
-		//screen on, disable_irq_wake
-		scr_suspended = false;
-		disable_irq_wake(client->irq);
-	}
-#endif
-
 	printk(KERN_INFO "[TP] %s: enter\n", __func__);
 	
 #ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_SWEEP2WAKE
@@ -2821,6 +2806,7 @@ static int synaptics_ts_resume(struct i2c_client *client)
 		input_report_abs(ts->input_dev, ABS_MT_AMPLITUDE, 0);
 		input_report_abs(ts->input_dev, ABS_MT_POSITION, 1 << 31);
 	}
+
 	
 #ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_SWEEP2WAKE
 	if (s2w_switch == 0) {
